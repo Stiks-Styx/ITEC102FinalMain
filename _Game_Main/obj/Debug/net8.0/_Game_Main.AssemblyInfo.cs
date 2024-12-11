47ca058e3d062c3517869e1de146a26d62da59fb
Stiks-Styx <aa1305589@gmail.com>
2024-12-11 11:37:42 +0800

    heh something new i guess
    Enemy
    Player Losing Life
    UI changes
    i forgot the other things

--- _Game_Main/obj/Debug/net8.0/_Game_Main.AssemblyInfo.cs
+++ _Game_Main/obj/Debug/net8.0/_Game_Main.AssemblyInfo.cs
@@ -13,7 +13,11 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("_Game_Main")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
+<<<<<<< Updated upstream
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7c98c4d4800d76d9029327cb748a1cad834ac782")]
+=======
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+40d3d0cc708d70bcc6359b6233093b8808b25918")]
+>>>>>>> Stashed changes
 [assembly: System.Reflection.AssemblyProductAttribute("_Game_Main")]
 [assembly: System.Reflection.AssemblyTitleAttribute("_Game_Main")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
